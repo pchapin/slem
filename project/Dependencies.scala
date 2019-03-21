a60fdca222636951a45411262db44842e527531f
Peter Chapin <chapinp@acm.org>
2019-03-21 13:51:23 -0400

    Added a dependency on scala-parser-combinators.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,12 +6,14 @@ object Dependencies {
   // Versions
   lazy val scalaTestVersion = "3.0.5"
   lazy val kiamaVersion = "2.2.0"
+  lazy val parserVersion = "1.1.1"
 
   // Libraries
   val scalactic = "org.scalactic" %% "scalactic" % scalaTestVersion
   val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
   val kiama = "org.bitbucket.inkytonik.kiama" %% "kiama" % kiamaVersion
+  val parser = "org.scala-lang.modules" %% "scala-parser-combinators" % parserVersion
 
   // Projects
-  val slemDeps = Seq(scalactic, kiama, scalaTest % Test)
+  val slemDeps = Seq(scalactic, kiama, parser, scalaTest % Test)
 }
